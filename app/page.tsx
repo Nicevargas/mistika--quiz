1b267af1b80477d1a0fb2362724b4574287245c8
Nice Vargas <eunicelvargas@gmail.com>
2026-03-18 18:49:25 -0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Quiz from '@/components/Quiz';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-[#000000] relative overflow-hidden">
-      <Quiz />
-    </main>
-  );
-}
